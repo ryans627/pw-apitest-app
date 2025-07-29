f837008d6aab2bae88ab3960d37b1528ba40e821
ryanshang <ryan.d.shang@gmail.com>
2025-07-29 15:54:56 -0400

    extract test data to a separate json file

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,9 +1,17 @@
 import { test, expect } from '@playwright/test';
+import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({ page }) => {
-    await page.goto('https://conduit.bondaracademy.com/')
+    await page.route('https://conduit-api.bondaracademy.com/api/tags', async route => {
+        await route.fulfill({
+            body: JSON.stringify(tags)
+        })
+    });
+
+    await page.goto('https://conduit.bondaracademy.com/');
 })
 
 test('project setup', async ({ page }) => {
     await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+    // await expect()
 })
